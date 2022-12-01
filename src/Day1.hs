8a155b5559f1bb5b7d3d5792ca4131bde70211e6
Anthony Brice <abrice@cj.com>
2022-12-01 10:44:23 -0800

    refactor

--- src/Day1.hs
+++ src/Day1.hs
@@ -3,12 +3,22 @@ module Day1 (day1) where
 import Data.List.Split (splitWhen)
 import Data.List (sortBy)
 
+type Elf = Integer
+
 day1 :: IO ()
 day1 = do
-  content <- readFile "input/day1a"
-  let xs' = lines content
-      xs = map (sum . map read) $ splitWhen (== "") xs'
-      day1a = maximum xs :: Integer
-  print day1a
-  let day1b = sum . take 3 $ sortBy (flip compare) xs
-  print day1b
+  input <- readFile "input/day1"
+  let xs = parseInputToElves input
+
+  print $ part1 xs
+  print $ part2 xs
+
+parseInputToElves :: String -> [Elf]
+parseInputToElves input = map (sum . map read) . splitWhen (== "") $ lines input
+
+part1 :: [Elf] -> Elf
+part1 = maximum
+
+part2 :: [Elf] -> Elf
+part2 xs = sum . take 3 $ sortBy (flip compare) xs
+
