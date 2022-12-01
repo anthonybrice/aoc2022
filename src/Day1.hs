4a5c3452531efd59ed79e32cf7f8aff9bf3b982d
Anthony Brice <abrice@cj.com>
2022-12-01 09:55:53 -0800

    fix warning

--- src/Day1.hs
+++ src/Day1.hs
@@ -8,7 +8,7 @@ day1 = do
   content <- readFile "input/day1a"
   let xs' = lines content
       xs = map (sum . map read) $ splitWhen (== "") xs'
-      day1a = maximum xs
+      day1a = maximum xs :: Integer
   print day1a
   let day1b = sum . take 3 $ sortBy (flip compare) xs
   print day1b
