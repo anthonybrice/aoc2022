3aefd7fdc891a818badea1c631708e34a351c854
Anthony Brice <abrice@cj.com>
2022-12-09 02:15:05 -0800

    day9 part2 done

--- src/Day9.hs
+++ src/Day9.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE LambdaCase #-}
-module Day9 (day9, singleMove, Direction(..)) where
+module Day9 (day9, Direction(..)) where
 
 import Prelude hiding (Right, Left)
 import Data.List (foldl', nub)
@@ -7,8 +7,17 @@ import Data.List (foldl', nub)
 day9 :: String -> String
 day9 input =
   let xs = map parseMove $ lines input
-      part1 = length . nub . map snd $ foldl' (\acc m -> trail (head acc) m <> acc) [((0,0),(0,0))] xs
-  in show part1
+      part1 = length
+        . nub
+        . map snd
+        $ foldl' (\acc m -> uncurry (trail m) (head acc) <> acc) [((0,0),(0,0))] xs
+
+      part2 = length
+        . nub
+        . map last
+        $ foldl' (\acc m -> ropeTrail m (head acc) <> acc) [replicate 10 (0,0)] xs
+
+  in show part1 <> "\n" <> show part2
 
 data Move = Move Direction Int
 
@@ -16,10 +25,9 @@ data Direction = Up | Down | Left | Right
   deriving Eq
 
 -- | Rope position
-type RP =
-  ( (Integer, Integer) -- | Head
-  , (Integer, Integer) -- | Tail
-  )
+type RP = (Integer, Integer)
+
+type Rope = [RP]
 
 parseMove :: String -> Move
 parseMove (x:' ':y) = Move (f x) (read y) where
@@ -31,26 +39,59 @@ parseMove (x:' ':y) = Move (f x) (read y) where
     _ -> error ""
 parseMove _ = error ""
 
-trail :: RP -> Move -> [RP]
-trail rp (Move d i) = foldl' (\acc d' -> singleMove (head acc) d' : acc) [rp] singleMoves
+ropeTrail :: Move -> Rope -> [Rope]
+ropeTrail _ [] = []
+ropeTrail (Move d i) rs = foldr (\d' acc -> singleMove' d' (head acc) : acc) [rs] singleMoves
   where
     singleMoves = if i > 1 then replicate i d else [d]
 
-singleMove :: RP -> Direction -> RP
-singleMove (h@(x, y),t@(x', y')) d
-  | x == x' + 1 && y == y' && d == Right = (move' h d, h)
-  | x == x' - 1 && y == y' && d == Left = (move' h d, h)
-  | x == x' && y == y' + 1 && d == Up = (move' h d, h)
-  | x == x' && y == y' - 1 && d == Down = (move' h d, h)
-  | x == x' - 1 && y == y' + 1 && (d == Up || d == Left) = (move' h d, h)
-  | x == x' + 1 && y == y' + 1 && (d == Up || d == Right) = (move' h d, h)
-  | x == x' + 1 && y == y' - 1 && (d == Down || d == Right) = (move' h d, h)
-  | x == x' - 1 && y == y' - 1 && (d == Down || d == Left) = (move' h d, h)
-  | otherwise = (move' h d, t)
+singleMove' :: Direction -> Rope -> Rope
+singleMove' _ [] = []
+singleMove' d (h : rs) =
+  let h' = move h d
+  in reverse $ foldl' (\acc r -> follow (head acc) r : acc) [h'] rs
 
-move' :: (Num a, Num b) => (a, b) -> Direction -> (a, b)
-move' (x,y) d' = case d' of
+trail :: Move -> RP -> RP -> [(RP, RP)]
+trail (Move d i) rp1 rp2 = foldr (\d' acc -> uncurry (singleMove d') (head acc) : acc) [(rp1, rp2)] singleMoves
+  where
+    singleMoves = if i > 1 then replicate i d else [d]
+
+singleMove :: Direction -> RP -> RP -> (RP, RP)
+singleMove d h@(x, y) t@(x', y')
+  | x == x' + 1 && y == y' && d == Right = (move h d, h)
+  | x == x' - 1 && y == y' && d == Left = (move h d, h)
+  | x == x' && y == y' + 1 && d == Up = (move h d, h)
+  | x == x' && y == y' - 1 && d == Down = (move h d, h)
+  | x == x' - 1 && y == y' + 1 && (d == Up || d == Left) = (move h d, h)
+  | x == x' + 1 && y == y' + 1 && (d == Up || d == Right) = (move h d, h)
+  | x == x' + 1 && y == y' - 1 && (d == Down || d == Right) = (move h d, h)
+  | x == x' - 1 && y == y' - 1 && (d == Down || d == Left) = (move h d, h)
+  | otherwise = (move h d, t)
+
+move :: RP -> Direction -> RP
+move (x,y) d' = case d' of
   Right -> (x+1,y)
   Up -> (x,y+1)
   Left -> (x-1,y)
   Down -> (x,y-1)
+
+-- | Given a moved head and the tail's current position, calculate the tail's next position
+follow :: RP -> RP -> RP
+follow (x, y) t@(x', y')
+  | x == x' - 2 && y == y' + 2 = (x' - 1, y' + 1)
+  | x == x' - 1 && y == y' + 2 = (x' - 1, y' + 1)
+  | x == x' && y == y' + 2 = (x', y' + 1)
+  | x == x' + 1 && y == y' + 2 = (x' + 1, y' + 1)
+  | x == x' + 2 && y == y' + 2 = (x' + 1, y' + 1)
+  | x == x' - 2 && y == y' + 1 = (x' - 1, y' + 1)
+  | x == x' + 2 && y == y' + 1 = (x' + 1, y' + 1)
+  | x == x' - 2 && y == y' = (x' - 1, y')
+  | x == x' + 2 && y == y' = (x' + 1, y')
+  | x == x' - 2 && y == y' - 1 = (x' - 1, y' - 1)
+  | x == x' + 2 && y == y' - 1 = (x' + 1, y' - 1)
+  | x == x' - 2 && y == y' - 2 = (x' - 1, y' - 1)
+  | x == x' - 1 && y == y' - 2 = (x' - 1, y' -  1)
+  | x == x' && y == y' - 2 = (x', y' - 1)
+  | x == x' + 1 && y == y' - 2 = (x' + 1, y' - 1)
+  | x == x' + 2 && y == y' - 2 = (x' +1, y' - 1)
+  | otherwise = t
--- test/Day9Spec.hs
+++ test/Day9Spec.hs
@@ -2,45 +2,17 @@ module Day9Spec (spec) where
 
 import Prelude hiding (Right, Left)
 import Test.Hspec
-import Day9 (day9, Direction(..), singleMove)
+import Day9 (day9)
 
 spec :: Spec
 spec = do
   describe "day9" $ do
-    it "solves the example input" $
-      day9 exampleInput `shouldBe` "13"
+    it "solves the example input" $ do
+      day9 exampleInput `shouldBe` "13\n1"
 
+      day9 exampleInput' `shouldBe` "88\n36"
 
-  describe "singleMove" $ do
-    it "works when the head covers the tail" $
-      map (singleMove ((0,0),(0,0))) ds `shouldBe` [((0,1),(0,0)), ((0,-1),(0,0)), ((-1,0),(0,0)), ((1,0),(0,0))]
-
-    it "works when the head is to just right of the tail" $
-      map (singleMove ((1,0),(0,0))) ds `shouldBe` [((1,1),(0,0)), ((1,-1),(0,0)), ((0,0),(0,0)), ((2,0),(1,0))]
-
-    it "works when the head is just left of the tail" $
-      map (singleMove ((-1,0),(0,0))) ds `shouldBe` [((-1,1),(0,0)), ((-1,-1),(0,0)), ((-2,0),(-1,0)), ((0,0),(0,0))]
-
-    it "works when the head is just above the tail" $
-      map (singleMove ((0,1),(0,0))) ds `shouldBe` [((0,2),(0,1)), ((0,0),(0,0)), ((-1,1),(0,0)), ((1,1),(0,0))]
-
-    it "works when the head is just below the tail" $
-      map (singleMove ((0,-1),(0,0))) ds `shouldBe` [((0,0),(0,0)), ((0,-2),(0,-1)), ((-1,-1),(0,0)), ((1,-1),(0,0))]
-
-    it "works when the head is above left of the tail" $
-      map (singleMove ((-1,1),(0,0))) ds `shouldBe` [((-1,2),(-1,1)), ((-1,0),(0,0)), ((-2,1),(-1,1)), ((0,1),(0,0))]
-
-    it "works when the head is above right of the tail" $
-      map (singleMove ((1,1),(0,0))) ds `shouldBe` [((1,2),(1,1)), ((1,0),(0,0)), ((0,1),(0,0)), ((2,1),(1,1))]
-
-    it "works when the head is below right of the tail" $
-      map (singleMove ((1,-1),(0,0))) ds `shouldBe` [((1,0),(0,0)), ((1,-2),(1,-1)), ((0,-1),(0,0)), ((2,-1),(1,-1))]
-
-    it "works when the head is below left of the tail" $
-      map (singleMove ((-1,-1),(0,0))) ds `shouldBe` [((-1,0),(0,0)), ((-1,-2),(-1,-1)), ((-2,-1),(-1,-1)), ((0,-1),(0,0))]
-
-ds = [Up, Down, Left, Right]
-
+exampleInput :: String
 exampleInput = init $ unlines
   [ "R 4"
   , "U 4"
@@ -50,5 +22,17 @@ exampleInput = init $ unlines
   , "D 1"
   , "L 5"
   , "R 2"
+  ]
+
+exampleInput' :: String
+exampleInput' = init $ unlines
+  [ "R 5"
+  , "U 8"
+  , "L 8"
+  , "D 3"
+  , "R 17"
+  , "D 10"
+  , "L 25"
+  , "U 20"
   ]
 
