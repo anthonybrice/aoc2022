c5e70ec8f467f9a483b2a38b4224756852630b06
Anthony Brice <abrice@cj.com>
2022-12-10 13:12:35 -0800

    refactor

--- src/Day10.hs
+++ src/Day10.hs
@@ -12,13 +12,13 @@ day10 input =
       part1 = sum
         . elems
         . snd
-        $ foldl' (\(a, s) instr -> runState (runInstr' a 1 instr) s) (Cpu 1 1, mempty) xs
+        $ foldl' (\(a, s) instr -> runState (runInstr a 1 mapStrengths instr) s) (Cpu 1 1, mempty) xs
 
       part2 = unlines
         . map reverse
         . reverse
         . snd
-        $ foldl' (\(a, s) instr -> runState (runInstr a 1 instr) s) (Cpu 1 1, mempty) xs
+        $ foldl' (\(a, s) instr -> runState (runInstr a 1 drawPixel instr) s) (Cpu 1 1, mempty) xs
 
   in show part1 <> "\n" <> part2
 
@@ -33,26 +33,16 @@ parseInstr xs = case words xs of
   ["addx", ys] -> Addx $ read ys
   _ -> error "bad parse"
 
-runInstr' :: Cpu Integer -> Int -> Instr Integer -> State (Map Integer Integer) (Cpu Integer)
-runInstr' (Cpu c x) tick instr = do
+mapStrengths :: Cpu Integer -> State (Map Integer Integer) ()
+mapStrengths (Cpu c x) = do
   m <- get
 
   if c `elem` [20, 60, 100, 140, 180, 220]
     then put $ insert c (x * c) m
     else put m
 
-  let cpu = case (instr, tick) of
-        (Noop, 1) -> Cpu (c+1) x
-        (Addx _, 1) -> Cpu (c+1) x
-        (Addx y, 2) -> Cpu (c+1) (x+y)
-        _ -> error "bad clock input"
-
-  case (instr, tick) of
-    (Addx _, 1) -> runInstr' cpu 2 instr
-    _ -> return cpu
-
-runInstr :: Cpu Integer -> Int -> Instr Integer -> State [String] (Cpu Integer)
-runInstr (Cpu c x) tick instr = do
+drawPixel :: Cpu Integer -> State [String] ()
+drawPixel (Cpu c x) = do
   ss <- get
 
   let i = (c - 1) `mod` 40
@@ -60,22 +50,18 @@ runInstr (Cpu c x) tick instr = do
 
   put $ (getPixel ((c - 1) `mod` 40) [x-1,x,x+1] : head ss') : tail ss'
 
-  let cpu = case (instr, tick) of
-        (Noop, 1) -> Cpu (c+1) x
-        (Addx _, 1) -> Cpu (c+1) x
-        (Addx y, 2) -> Cpu (c+1) (x+y)
-        _ -> error "bad clock input"
+runInstr :: Cpu Integer -> Int -> (Cpu Integer -> State a b) -> Instr Integer -> State a (Cpu Integer)
+runInstr cpu@(Cpu c x) tick f instr = do
+  _ <- f cpu
 
   case (instr, tick) of
-    (Addx _, 1) -> runInstr cpu 2 instr
-    _ -> return cpu
+        (Noop, 1) -> return $ Cpu (c+1) x
+        (Addx _, 1) -> runInstr cpu 2 f instr
+        (Addx y, 2) -> return $ Cpu (c+1) (x+y)
+        _ -> error "bad clock input"
 
 getPixel :: Integer -> [Integer] -> Char
 getPixel p sprite =
   if p `elem` sprite
     then '#'
     else '.'
-
-
-
-
